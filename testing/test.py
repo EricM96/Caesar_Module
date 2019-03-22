7e786b2579163d20d353e25b382056691b59f478
Eric McCullough <eric96@live.missouristate.edu>
2019-03-22 13:32:15 -0500

    Add comments to test.py

--- testing/test.py
+++ testing/test.py
@@ -1,11 +1,15 @@
+# Importing classes in Python can be rather tricky. You have to first add the directory the file is
+# stored in to Pythons path variable. The following two line demonstrates how this can be done.
 import sys
 sys.path.append('../deliverable')
 from caesarCrypt import CaesarCrypt
 
 def main():
+    # the initialization arguement is the shift key. It can be any number between 1-25
     crypt = CaesarCrypt(25)
     test_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     print(test_string)
+    # the C code encrypts and decrypts strings in place 
     crypt.encrypt(test_string)
     print(test_string)
     crypt.decrypt(test_string)
